f0d758fda9daf3cf658cd6cabd2c9dd434697fbe
2pushkaraj3 <85059599+2pushkaraj3@users.noreply.github.com>
2023-05-15 16:09:23 +0530

    Deploy React app to GitHub Pages

--- codecg/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Button} from "@material-ui/core"
-const Button = ({ styles }) => (
-  <Button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-    Get Started
-  </Button>
-);
-
-export default Button;
